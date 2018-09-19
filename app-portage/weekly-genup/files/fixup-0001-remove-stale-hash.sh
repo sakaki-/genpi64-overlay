ae131dab7c8a96cc3b787315fe92132f741e567c
sakaki <sakaki@deciban.com>
2018-09-19 17:52:21 +0100

    Add app-portage/weekly-genup-1.1.0.ebuild
    Includes a new weekly cron entry for fixup scripts (a way to handle
    situations where e.g. the main tree sync gets blocked due to
    earlier configuration errors).

---
+++ app-portage/weekly-genup/files/fixup-0001-remove-stale-hash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# fixup-0001-remove-stale-hash.sh
+#
+# Remove any repo.hash{,.asc} files in /usr/portage/local that are
+# older than their versions in /usr/portage, if they exist (can cause
+# sync to fail)
+#
+# Copyright (c) 2018 sakaki <sakaki@deciban.com>
+# License: GPL v3+
+# NO WARRANTY
+
+PDIR="$(portageq get_repo_path / gentoo)"
+PLDIR=${PDIR}/local
+
+for F in repo.hash{,.asc}; do
+    if [[ -f "${PDIR}/${F}" && -f "${PLDIR}/${F}" && "${PLDIR}/${F}" -ot "${PDIR}/${F}" ]]; then
+        echo "Removing stale ${PDIR}/${F}"
+        rm -fv "${PLDIR}/${F}"
+    fi
+done
