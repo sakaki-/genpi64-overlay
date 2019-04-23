a411c8715542b89d6f29dd8a374408d81e632ecf
sakaki <sakaki@deciban.com>
2019-04-24 00:35:11 +0100

    Add fixup-0005-ensure-hwclock-disabled.sh
    Ensure hardware RTC is always disabled, as not present on RPi3
    Role is performed by swclock service instead, plus chrony (NTP)

---
+++ app-portage/weekly-genup/files/fixup-0005-ensure-hwclock-disabled.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# fixup-0005-ensure-hwclock-disabled.sh
+#
+# The RPi3 has now battery-backed RTC, so the hwclock
+# service will fail if called. Ensure it is disabled.
+#
+# Copyright (c) 2019 sakaki <sakaki@deciban.com>
+# License: GPL v3+
+# NO WARRANTY
+
+echo "Ensuring hwclock service is disabled"
+if [[ -L /etc/runlevels/boot/hwclock ]]; then
+    rc-update del hwclock boot
+fi
