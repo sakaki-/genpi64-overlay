61d2c4933c78e0e05bf6090feb5c566743435748
sakaki <sakaki@deciban.com>
2019-05-26 17:58:43 +0100

    Add fixup for linux-firmware license changes.

---
+++ app-portage/weekly-genup/files/fixup-0006-linux-firmware-licenses.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# fixup-0006-linux-firmware-licenses.sh
+#
+# Ensure that appropriate licenses are enabled for the linux-firmware
+# package (account for new linux-fw-redistributable license).
+#
+# Copyright (c) 2019 sakaki <sakaki@deciban.com>
+# License: GPL v3+
+# NO WARRANTY
+
+LF="/etc/portage/package.license/linux-firmware"
+echo "Ensuring linux-firmware allowed licenses are up-to-date"
+
+if [[ -s "${LF}" ]]; then
+    if ! grep -q "linux-fw-redistributable" 2>/dev/null "${LF}"; then
+        sed -e 's/^sys-kernel/linux-firmware freedist/sys-kernel/linux-firmware freedist linux-fw-redistributable/' "${LF}"
+    fi
+fi
