25bd01f74ffb80a3563f6a47c3a3a73699f614ed
sakaki <sakaki@deciban.com>
2020-04-03 22:09:57 +0100

    Add fixup-0011 to depclean app-portage/porthole (bug #708096)
    It is blocking upgrades to sys-apps/portage by requiring a python2
    target, which portage no longer supports (now python 2.7 is EOL).
    
    You _can_ still manually reinstall if you want (the depclean is only
    tried once); but you'll get stuck with an old version of portage if you do.

---
+++ app-portage/weekly-genup/files/fixup-0011-depclean-porthole.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# fixup-0011-depclean-porthole.sh
+#
+# The Portage front end porthole has been tree-cleaned (Gentoo bug #708096).
+# The last-known-good ebuild has been migrated to the sakaki-tools repo,
+# but is now preventing sys-apps/portage from upgrading. Accordingly, this
+# fixup depcleans it. A sentinel is written so the depclean is only tried once;
+# if the user re-installs porthole, it will stick.
+#
+# Copyright (c) 2020 sakaki <sakaki@deciban.com>
+# License: GPL v3+
+# NO WARRANTY
+
+PCDIR="/etc/portage"
+SENTINEL="${PCDIR}/.fixup-0011-done"
+
+if [[ -f "${SENTINEL}" ]]; then
+    echo "Sentinel file found: porthole already deselected"
+    exit 0
+fi
+
+echo "Depcleaning app-portage/porthole (bug #708096)"
+emerge --depclean app-portage/porthole
+
+echo -e "This sentinel file prevents fixup-0011 from trying to depclean\napp-portage/porthole a second time." > "${SENTINEL}"
